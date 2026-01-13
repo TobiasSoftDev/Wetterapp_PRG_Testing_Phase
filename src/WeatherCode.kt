b09601c61aa7eba72dc1692bfa44cf27c20afabb
Patrick <patrick.theiler@stud.abbts.ch>
2026-01-13 21:38:24 +0100

    Favoriten Objekt hinzugefügt und Klassendiagramm erweitert

--- src/WeatherCode.kt
+++ src/WeatherCode.kt
@@ -4,7 +4,7 @@ enum class WeatherCode(val code: Int, val beschreibung: String, val icon: String
     SONNIG(0, "Sonne", "☀️"),
     LEICHT_BEWOELKT(1-3, "leicht bewölkt", "⛅"),
     BEWOELKT(3, "Bewölkt", "☁️"),
-    NEBEL(40-49 , "Nebel", "🌫️"),
+    NEBEL(code = 40-49, "Nebel", "🌫️"),
     LEICHTER_REGEN(50-59+80-90, "leichter Regen", "🌦️"),
     REGEN(60-69, "Regen", "🌧️"),
     SCHNEE(71, "Schnee", "🌨️"),
