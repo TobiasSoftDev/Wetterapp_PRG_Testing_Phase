cd00af20255f1eaf365cf78462e7e0a87a4cb73b
Tobias Graf <tobias.graf@stud.abbts.ch>
2026-02-16 22:25:30 +0100

    Main angepasst

--- src/Main.kt
+++ src/Main.kt
@@ -13,41 +13,7 @@ import javafx.application.Application.*
 
 fun main(){
 
-    //    val favorites = Favorite(
-//        location = Location(0.0, 0.0, "default", "default", "default", "default", 0.0, 0U),
-//        name = "Default",
-//        temperature = 0.0,
-//        icon =  Image("")
-//        )
-
-   /* val weather = Weather(
-        location = Location(0.0, 0.0, "default", "default", "default", "default", 0.0, 0U),
-        temperature = 0.0,
-        humidity = 0,
-        weatherCode = WeatherCodes.UNBEKANNT,
-        precipitation = 0,
-        windSpeed = 0,
-        windDirection = 0,
-        apparentTemperature = 0.0,
-        hourlyList = listOf<HourlyData>(),
-        dailyList = listOf<DailyData>()
-    ) */
-
-    //    val favorites = Favorite(
-//        location = Location(0.0, 0.0, "default", "default", "default", "default", 0.0, 0U),
-//        name = "Default",
-//        temperature = 0.0,
-//        icon =  Image("")
-//        )
-
     launch(Gui::class.java)
-
-//    val storage: Storabledata = WeatherData()
-//
-//    println("Daily: ${storage.readWeatherDataDaily()}")
-//    println("Hourly: ${storage.readWeatherDataHourly()}")
-//    println("Current: ${storage.readWeatherData()}")
-//    println("Favorite: ${storage.readFavorites()}")
 
 //    val myApiHandler = ApiHandler()
 //    val myTest = Manager(myApiHandler)
